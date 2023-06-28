94ae8d7f3734821c935758d8874d92757e7a6f18
badblues <borovikxv.a@gmail.com>
2023-06-28 23:59:54 +0700

    login semi-working

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
+
+  constructor(private router: Router) {}
+
+  onClick() {
+    console.log("huy");
+    this.router.navigate(['/login']);
+  }
 
 }
