b968fa6892cfb7cff7ea3189ca68e35535fba917
badbadblues _ <borovikxv.a@gmail.com>
2023-10-23 19:07:31 +0700

    add TaskApiService

---
+++ src/services/TasksApiService.js
@@ -0,0 +1,46 @@
+import http from "axios";
+
+export default class TasksApiService {
+  apiUrl = "http://212.20.47.147:8080/api/v2/tasks";
+
+  async getTasks() {
+    let url = this.apiUrl;
+    try {
+      const response = await http.get(url);
+      return response.data.data;
+    } catch (error) {}
+  }
+
+  async getTask(id) {
+    let url = this.apiUrl + `/${id}`;
+    try {
+      const response = await http.get(url);
+      return response.data.data;
+    } catch (error) {}
+  }
+
+  async createTask(task) {
+    let url = this.apiUrl;
+    try {
+      const response = await http.post(url, task);
+      return response.data.data;
+    } catch (error) {}
+  }
+
+  async updateTask(id, task) {
+    let url = this.apiUrl + `/${id}`;
+    try {
+      const response = await http.put(url, task);
+      return response.data.data;
+    } catch (error) {}
+  }
+
+  async deleteTask(id) {
+    let url = this.apiUrl + `/${id}`;
+    try {
+      const response = await http.delete(url);
+      return response.data.data;
+    } catch (error) {}
+  }
+
+}
