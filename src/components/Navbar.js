9dcf9a776406f7a910d07592f24d614c5aa63983
badbadblues _ <borovikxv.a@gmail.com>
2023-12-10 15:56:52 +0700

    add group label in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,43 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { UserContext } from "../contexts/UserContext";
 import { UiContext } from "../contexts/UiContext";
+import { ApiContext } from "../contexts/ApiContext";
 import { useNavigate } from "react-router-dom";
 import { Roles } from "../models/Roles";
 import DarkModeToggle from "./DarkModeToggle";
 import AdminMenu from "./menus/AdminMenu";
 import TeacherMenu from "./menus/TeacherMenu";
 import StudentMenu from "./menus/StudentMenu";
+import Loader from "./Loader";
 
 const Navbar = () => {
   const { user, logout } = useContext(UserContext);
   const { darkMode } = useContext(UiContext);
+  const [loading, setLoading] = useState(true);
+  const [group, setGroup] = useState([]);
+  const { studentApiService } = useContext(ApiContext);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (user.role === Roles.Student) {
+        const student = await studentApiService.getStudent(user.id);
+        setGroup(student.group);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="disciplines-container">
+        <Loader />
+      </div>
+    );
+  }
 
   const menus = {
     [Roles.Admin]: AdminMenu,
@@ -45,7 +69,12 @@ const Navbar = () => {
           <div className="menu">{Menu && <Menu />}</div>
           <div className="profile">
             {user.loggedIn ? (
-              <label className="username clickable">{user.fullName}</label>
+              <div className="mini-profile">
+                <label className="username clickable">{user.fullName}</label>
+                {user.role === Roles.Student ? (
+                  <label>{group.name}</label>
+                ) : null}
+              </div>
             ) : null}
             {user.loggedIn ? (
               <label className="logout clickable" onClick={onLogout}>
