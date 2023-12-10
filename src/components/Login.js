a47aec98aa88409b4214534eec9f1fd1d61fc5fa
badbadblues _ <borovikxv.a@gmail.com>
2023-12-10 16:55:29 +0700

    add show password button

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,6 +11,7 @@ const Login = () => {
   const { login } = useContext(UserContext);
   const navigate = useNavigate();
   const { register, handleSubmit, formState } = useForm();
+  const [showPassword, setShowPassword] = useState(false);
   const { errors } = formState;
   const [loading, setLoading] = useState(false);
 
@@ -53,7 +54,7 @@ const Login = () => {
             </label>
             <input
               className={`form-input ${darkMode ? "dark-mode" : ""}`}
-              type="password"
+              type={`${showPassword ? "text" : "password"}`}
               placeholder="password..."
               {...register("password", {
                 required: "Необходимо ввести пароль",
@@ -62,6 +63,13 @@ const Login = () => {
             <label className={`form-text ${darkMode ? "dark-mode" : ""}`}>
               {errors.password?.message}
             </label>
+            <button
+              type="button"
+              onClick={() => {setShowPassword(!showPassword)}}
+              className="button" 
+            >
+              {`${showPassword ? "Скрыть пароль" : "Показать пароль"}`}
+            </button>
           </div>
           <img src="/images/logo.png" alt="logo" width="100px"></img>
           <button disabled={loading} className="button form-button">
