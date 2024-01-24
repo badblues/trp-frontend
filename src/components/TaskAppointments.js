c2aba3fcd74ecf6b78b96a42a92d5f9fbda5c17e
badbadblues _ <borovikxv.a@gmail.com>
2024-01-24 18:01:41 +0700

    add task-appointments component

---
+++ src/components/TaskAppointments.js
@@ -0,0 +1,31 @@
+import React, { useContext, useState } from 'react';
+import './TaskAppointments.css';
+import { UiContext } from '../contexts/UiContext';
+
+const TaskAppointments = ({tasks, onTaskClick}) => {
+  const [taskList, setTaskList] = useState(tasks);
+  const { darkMode } = useContext(UiContext);
+
+  const handleTaskChangeState = (task) => {
+    const updatedTasks = [...taskList];
+    updatedTasks.forEach(t => {
+      if (t.id == task.id)
+        t.appointed = task.appointed;
+    });
+    setTaskList(updatedTasks);
+  }
+
+  return (
+    <div className='task-appointments-container'>
+      {taskList.map((task) => (
+        <div
+          onClick={() => {onTaskClick(task, handleTaskChangeState)}}
+          key={task.id}>
+            <h4 className={`task-appointements-item status-${task.appointed ? 'appointed' : 'not-appointed'} ${darkMode ? "dark-mode" : ""}`}>{task.title}</h4>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default TaskAppointments;
