042d354087be378aa31f92bd3d179881d590eca8
badbadblues _ <borovikxv.a@gmail.com>
2024-02-29 21:58:32 +0700

    change senior teacher role constant

--- src/models/Roles.js
+++ src/models/Roles.js
@@ -2,5 +2,5 @@ export const Roles = {
   Admin: "ROLE_ADMIN",
   Student: "ROLE_STUDENT",
   Teacher: "ROLE_TEACHER",
-  SeniorTeacher: "ROLE_SENIOR_TEACHER",
+  SeniorTeacher: "ROLE_LECTURE_TEACHER",
 }
