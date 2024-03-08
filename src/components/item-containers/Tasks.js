edb23a76871b9626236fe570666fb3b71836b430
badbadblues _ <borovikxv.a@gmail.com>
2024-03-08 18:52:25 +0700

    make tasks container dumb

--- src/components/item-containers/Tasks.js
+++ src/components/item-containers/Tasks.js
@@ -1,43 +1,20 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext } from "react";
 import { UiContext } from "../../contexts/UiContext";
-import { ApiContext } from "../../contexts/ApiContext";
 import "./Tasks.css";
-import FakeItemsList from "../loaders/FakeItemsList";
 
-const Tasks = ({ disciplineId, onSelect }) => {
-  const [tasks, setTasks] = useState([]);
-  const [loading, setLoading] = useState(true);
+const Tasks = ({ tasks, onSelect }) => {
   const { darkMode } = useContext(UiContext);
-  const { taskApiService } = useContext(ApiContext);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const tasksResponse =
-        await taskApiService.getTasksByDiscipline(disciplineId);
-      tasksResponse.sort((a, b) => {
-        const nameA = a.title.toLowerCase();
-        const nameB = b.title.toLowerCase();
-        if (nameA < nameB) {
-          return -1; // a should come before b
-        }
-        if (nameA > nameB) {
-          return 1; // b should come before a
-        }
-        return 0; // names are equal
-      });
-      setTasks(tasksResponse);
-      setLoading(false);
-    };
-    fetchData();
-  }, []);
-
-  if (loading) {
-    return (
-      <div>
-        <FakeItemsList />
-      </div>
-    );
-  }
+  tasks.sort((a, b) => {
+    const nameA = a.title.toLowerCase();
+    const nameB = b.title.toLowerCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
 
   return (
     <div className="tasks-container">
