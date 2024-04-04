67d8c3f95236358085f61dcb87e9d6bd0d0292ee
badbadblues _ <borovikxv.a@gmail.com>
2024-04-04 10:35:06 +0700

    add variants to a lab-work model

--- src/models/domain/LabWork.ts
+++ src/models/domain/LabWork.ts
@@ -1,6 +1,9 @@
+import { LabWorkVariant } from "./LabWorkVariant";
+
 export interface LabWork {
   id: number;
   disciplineId: number;
   title: string;
   maxRating: number;
+  variants: LabWorkVariant[];
 }
