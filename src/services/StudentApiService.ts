fb3192f3983fa9531f511beebb77eac5a2112be4
badbadblues _ <borovikxv.a@gmail.com>
2024-04-05 14:39:51 +0700

    fix getStudentsByGroup method

--- src/services/StudentApiService.ts
+++ src/services/StudentApiService.ts
@@ -3,6 +3,7 @@ import { Student } from "../models/domain/Student";
 
 export default class StudentApiService {
   apiUrl = "http://212.20.47.147:8080/api/v2/students";
+  groupApiUrl = "http://212.20.47.147:8080/api/v2/groups";
 
   async getStudents(): Promise<Array<Student>> {
     let url = this.apiUrl;
@@ -24,8 +25,13 @@ export default class StudentApiService {
     }
   }
 
-  async getStudentsByGroup(groupId: number): Promise<Array<Student>> {
-    const students = await this.getStudents();
-    return students.filter((s) => (s.group.id = groupId));
+  async getStudentsByGroup(groupId: number): Promise<Student[]> {
+    let url = this.groupApiUrl + `/${groupId}/students`;
+    try {
+      const response = await http.get(url);
+      return response.data.data;
+    } catch (error) {
+      throw error.response.data;
+    }
   }
 }
