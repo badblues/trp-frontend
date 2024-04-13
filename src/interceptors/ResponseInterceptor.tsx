579beb8ba562e02a61f51f00d1b74084dfbd1a3d
badbadblues _ <borovikxv.a@gmail.com>
2024-04-13 16:26:29 +0700

    remove console outputs

--- src/interceptors/ResponseInterceptor.tsx
+++ src/interceptors/ResponseInterceptor.tsx
@@ -1,7 +1,5 @@
 export const responseErrorInterceptor = (error, logout, navigate) => {
-  console.log(error);
   if (error.response) {
-    console.log(error.response);
     switch (error.response.status) {
       case 401:
         logout();
