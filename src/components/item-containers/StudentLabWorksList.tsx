81ddda099e7624bcaf54e715f593d3d9eca72e05
badbadblues _ <borovikxv.a@gmail.com>
2024-04-21 16:02:15 +0700

    replace lab-work-variants with team-appointments

--- src/components/item-containers/StudentLabWorksList.tsx
+++ src/components/item-containers/StudentLabWorksList.tsx
@@ -8,14 +8,17 @@ import editImg from "../../images/edit.png";
 import plusImg from "../../images/plus.png";
 import "../../styles/lab-works-list.css";
 import { LabWorkVariant } from "../../models/domain/LabWorkVariant.ts";
+import { TeamAppointment } from "../../models/domain/TeamAppointment.ts";
 
 interface Props {
   labWorks: LabWork[];
+  teamAppointments: TeamAppointment[];
   onLabWorkVariantSelect: (labWorkVariant: LabWorkVariant) => void;
 }
 
 const StudentLabWorksList: React.FC<Props> = ({
   labWorks,
+  teamAppointments,
   onLabWorkVariantSelect,
 }) => {
   const { theme } = useContext(UiContext) as UiContextType;
@@ -46,45 +49,49 @@ const StudentLabWorksList: React.FC<Props> = ({
 
   return (
     <div className={`lab-work-list ${theme}`}>
-      {labWorks.map((labWork) => (
-        <div className="lab-work" key={labWork.id}>
-          <div className="lab-work-container">
-            <div
-              className="title-container"
-              onClick={() => onLabWorkSelect(labWork)}
-            >
-              {openLabWorks.has(labWork) ? (
-                <img
-                  className="arrow"
-                  src={downArrow}
-                  width="13px"
-                  height="13px"
-                />
-              ) : (
-                <img
-                  className="arrow"
-                  src={rightArrow}
-                  width="13px"
-                  height="13px"
-                />
-              )}
-              <h4 className="title">{labWork.title}</h4>
+      {labWorks.map((labWork) =>
+        teamAppointments.some(
+          (tA) => tA.labWorkVariant.labWorkId === labWork.id
+        ) ? (
+          <div className="lab-work" key={labWork.id}>
+            <div className="lab-work-container">
+              <div
+                className="title-container"
+                onClick={() => onLabWorkSelect(labWork)}
+              >
+                {openLabWorks.has(labWork) ? (
+                  <img
+                    className="arrow"
+                    src={downArrow}
+                    width="13px"
+                    height="13px"
+                  />
+                ) : (
+                  <img
+                    className="arrow"
+                    src={rightArrow}
+                    width="13px"
+                    height="13px"
+                  />
+                )}
+                <h4 className="title">{labWork.title}</h4>
+              </div>
             </div>
+            {openLabWorks.has(labWork)
+              ? labWork.variants.map((variant) => (
+                  <div className="variant" key={variant.id}>
+                    <p
+                      onClick={() => onLabWorkVariantSelect(variant)}
+                      className="variant-title"
+                    >
+                      {variant.title}
+                    </p>
+                  </div>
+                ))
+              : null}
           </div>
-          {openLabWorks.has(labWork)
-            ? labWork.variants.map((variant) => (
-                <div className="variant" key={variant.id}>
-                  <p
-                    onClick={() => onLabWorkVariantSelect(variant)}
-                    className="variant-title"
-                  >
-                    {variant.title}
-                  </p>
-                </div>
-              ))
-            : null}
-        </div>
-      ))}
+        ) : null
+      )}
     </div>
   );
 };
--- src/components/pages/discipline/StudentDisciplinePage.tsx
+++ src/components/pages/discipline/StudentDisciplinePage.tsx
@@ -8,6 +8,11 @@ import { Discipline } from "../../../models/domain/Discipline.ts";
 import { LabWork } from "../../../models/domain/LabWork.ts";
 import { Teacher } from "../../../models/domain/Teacher.ts";
 import StudentLabWorksList from "../../item-containers/StudentLabWorksList.tsx";
+import { TeamAppointment } from "../../../models/domain/TeamAppointment.ts";
+import { Role } from "../../../models/domain/Role.ts";
+import { TeamAppointmentStatus } from "../../../models/domain/TeamAppointmentStatus.ts";
+import { Language } from "../../../models/domain/Language.ts";
+import { CType } from "../../../models/domain/Type.ts";
 
 interface Props {
   defaultDiscipline: Discipline;
@@ -19,16 +24,15 @@ const StudentDisciplinePage: React.FC<Props> = ({ defaultDiscipline }) => {
   const {
     teacherAppointmentApiService,
     labWorkApiService,
-    labWorkVariantApiService,
+    teamAppointmentApiService,
   } = useContext(ApiContext) as ApiContextType;
   const discipline = defaultDiscipline;
   const [loading, setLoading] = useState<boolean>(true);
   const [teachers, setTeachers] = useState<Teacher[]>([]);
+  const [teamAppointments, setTeamAppointments] = useState<TeamAppointment[]>(
+    []
+  );
   const [labWorks, setLabWorks] = useState<LabWork[]>([]);
-
-  const navigateToLabWork = (labWork: LabWork) => {
-    navigate(`/lab-work/${labWork.id}`);
-  };
 
   useEffect(() => {
     (async () => {
@@ -37,13 +41,101 @@ const StudentDisciplinePage: React.FC<Props> = ({ defaultDiscipline }) => {
           await teacherAppointmentApiService.getAppointmentsByDiscipline(
             discipline.id
           );
-        const labWorksResponse =
-          await labWorkApiService.getLabWorksByDiscipline(discipline.id);
         const teachers = teacherApointments.map(
           (appointment) => appointment.teacher
         );
         setTeachers(teachers);
-        setLabWorks(labWorksResponse);
+        //TEMPORARY
+        setLabWorks([
+          {
+            id: 1,
+            title: "LR 1",
+            disciplineId: 1,
+            maxRating: 100,
+            variants: [
+              {
+                id: 1,
+                labWorkId: 1,
+                title: "1. Функция суммы",
+                description:
+                  "Создайте функцию add, которая принимает два аргумента (числа) и возвращает их сумму.",
+                language: Language.C,
+                testable: true,
+                functionName: "add",
+                returnType: CType.Int,
+                arguments: [
+                  {
+                    name: "a",
+                    type: "int",
+                  },
+                  {
+                    name: "b",
+                    type: "int",
+                  },
+                ],
+                inputRegex: "",
+                outputRegex: "",
+              },
+            ],
+          },
+        ]);
+        //TEMPORARY
+        setTeamAppointments([
+          {
+            id: 1,
+            team: {
+              id: 18,
+              disciplineId: 1,
+              students: [
+                {
+                  id: 5,
+                  group: {
+                    id: 1,
+                    name: "AVTTEMPORARY",
+                  },
+                  fullName: "ALEXEY",
+                  username: "username",
+                  role: Role.Student,
+                },
+                {
+                  id: 38,
+                  group: {
+                    id: 1,
+                    name: "AVTTEMPORARY",
+                  },
+                  fullName: "asdf",
+                  username: "username",
+                  role: Role.Student,
+                },
+              ],
+            },
+            status: TeamAppointmentStatus.InProgress,
+            labWorkVariant: {
+              id: 1,
+              labWorkId: 1,
+              title: "1. Функция суммы",
+              description:
+                "Создайте функцию add, которая принимает два аргумента (числа) и возвращает их сумму.",
+              language: Language.C,
+              testable: true,
+              functionName: "add",
+              returnType: CType.Int,
+              arguments: [
+                {
+                  name: "a",
+                  type: "int",
+                },
+                {
+                  name: "b",
+                  type: "int",
+                },
+              ],
+              inputRegex: "",
+              outputRegex: "",
+            },
+            codeReviewIds: [],
+          },
+        ]);
       } catch (error) {
         showErrorAlert(error.error);
       }
@@ -77,6 +169,7 @@ const StudentDisciplinePage: React.FC<Props> = ({ defaultDiscipline }) => {
         <h2>Доступные лабораторные работы:</h2>
         <StudentLabWorksList
           labWorks={labWorks}
+          teamAppointments={teamAppointments}
           onLabWorkVariantSelect={(variant) =>
             navigate(`/lab-work-variants/${variant.id}`)
           }
