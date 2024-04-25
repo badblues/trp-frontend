091d85a908654710882b322313a9d4c613797719
badbadblues _ <borovikxv.a@gmail.com>
2024-04-25 21:09:27 +0700

    remover temporary stuff

--- src/components/pages/code-review/StudentCodeReviewPage.tsx
+++ src/components/pages/code-review/StudentCodeReviewPage.tsx
@@ -8,16 +8,20 @@ import ReactCodeMirror from "@uiw/react-codemirror";
 import { dracula } from "@uiw/codemirror-theme-dracula";
 import { java } from "@codemirror/lang-java";
 import { TeamAppointment } from "../../../models/domain/TeamAppointment.ts";
-import { TeamAppointmentStatus } from "../../../models/domain/TeamAppointmentStatus.ts";
-import { Language } from "../../../models/domain/Language.ts";
-import { CType } from "../../../models/domain/Type.ts";
 import PageWithTabs from "../../PageWithTabs.tsx";
-import StudentTestList from "../../item-containers/StudentTestList.tsx";
 import Loader from "../../Loader.tsx";
+import { ApiContext, ApiContextType } from "../../../contexts/ApiContext.tsx";
 import { LabWorkVariantTest } from "../../../models/domain/LabWorkVariantTest.ts";
+import StudentTestList from "../../item-containers/StudentTestList.tsx";
 
 const StudentCodeReviewPage = () => {
+  const { disciplineId, teamAppointmentId, codeReviewId } = useParams();
   const { theme, showErrorAlert } = useContext(UiContext) as UiContextType;
+  const {
+    teamAppointmentApiService,
+    codeReviewApiService,
+    labWorkVariantTestApiService,
+  } = useContext(ApiContext) as ApiContextType;
   const navigate = useNavigate();
   const [loading, setLoading] = useState<boolean>(true);
   const [codeReview, setCodeReview] = useState<CodeReview>();
@@ -28,93 +32,29 @@ const StudentCodeReviewPage = () => {
   useEffect(() => {
     (async () => {
       try {
-        setTests([
-          {
-            id: 5,
-            labWorkVariantId: 1,
-            input: "1, 2",
-            output: "3",
-            open: true,
-          },
-          {
-            id: 7,
-            labWorkVariantId: 1,
-            input: "1, 2",
-            output: "3",
-            open: true,
-          },
-        ]);
-        setTeamAppointment({
-          id: 1,
-          team: {
-            id: 18,
-            disciplineId: 1,
-            students: [
-              {
-                id: 5,
-                group: {
-                  id: 1,
-                  name: "AVTTEMPORARY",
-                },
-                fullName: "ALEXEY",
-                username: "username",
-                role: Role.Student,
-              },
-              {
-                id: 38,
-                group: {
-                  id: 1,
-                  name: "AVTTEMPORARY",
-                },
-                fullName: "asdf",
-                username: "username",
-                role: Role.Student,
-              },
-            ],
-            leaderId: 38,
-          },
-          status: TeamAppointmentStatus.InProgress,
-          labWorkVariant: {
-            id: 1,
-            labWorkId: 1,
-            title: "1. Функция суммы",
-            description:
-              "Создайте функцию add, которая принимает два аргумента (числа) и возвращает их сумму.",
-            language: Language.C,
-            testable: true,
-            functionName: "add",
-            returnType: CType.Int,
-            arguments: [
-              {
-                name: "a",
-                type: "int",
-              },
-              {
-                name: "b",
-                type: "int",
-              },
-            ],
-            inputRegex: "",
-            outputRegex: "",
-          },
-          codeReviewIds: [],
-        });
-        setCodeReview({
-          id: 1,
-          code: "int func(int a, int b) {\n\treturn a + b;\n}",
-          taskMessages: [
-            {
-              message: "Hello",
-              roleType: Role.Student,
-            },
-            {
-              message: "Hellor there ",
-              roleType: Role.Teacher,
-            },
-          ],
-          codeThreads: [],
-        });
+        const teamAppointments =
+          await teamAppointmentApiService.getTeamAppointmentsByDiscipline(
+            Number(disciplineId)
+          );
+        const teamAppointment = teamAppointments.find(
+          (tA) => tA.id === Number(teamAppointmentId)
+        );
+        if (teamAppointment === undefined) {
+          navigate("/not-found");
+          return;
+        }
+        const codeReviewResponse = await codeReviewApiService.getCodeReview(
+          Number(codeReviewId)
+        );
+        const testsResponse =
+          await labWorkVariantTestApiService.getOpenLabWorkVariantTestsByLabWorkVariant(
+            teamAppointment.labWorkVariant.id
+          );
+        setTests(testsResponse);
+        setCodeReview(codeReviewResponse);
+        setTeamAppointment(teamAppointment);
       } catch (error) {
+        console.log(error, "EROEROEJOR");
         showErrorAlert(error.error);
         if (error.status === 404) navigate("/not-found");
       }
@@ -156,14 +96,14 @@ const StudentCodeReviewPage = () => {
             </div>
             <div className="chat-container">
               <div className="chat-messages">
-                {codeReview?.taskMessages.map((taskMessage) => (
+                {codeReview?.messages.map((taskMessage) => (
                   <p
                     className={`message ${
-                      taskMessage.roleType === Role.Student ? "right" : "left"
+                      taskMessage.user.role === Role.Student ? "right" : "left"
                     }`}
                   >
                     <p>
-                      {taskMessage.roleType === Role.Student
+                      {taskMessage.user.role === Role.Student
                         ? "Студенты:"
                         : "Преподаватель:"}
                     </p>
@@ -207,7 +147,7 @@ const StudentCodeReviewPage = () => {
                   )
                 </h2>
                 <h2>Примеры тестов:</h2>
-                <StudentTestList tests={tests!} />
+                <StudentTestList tests={tests} />
               </>
             ) : null}
           </div>
