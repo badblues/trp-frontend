eaa1670773389ab1c0238061facf6857a0008352
badbadblues _ <borovikxv.a@gmail.com>
2024-04-26 20:45:19 +0700

    fix sendMessage method

--- src/services/CodeReviewApiService.ts
+++ src/services/CodeReviewApiService.ts
@@ -1,6 +1,7 @@
 import http from "axios";
 import { CodeReview } from "../models/domain/CodeReview";
 import urls from "./urls.ts";
+import { CodeReviewMessageDTO } from "../models/DTO/CodeReviewMessageDTO.ts";
 
 export class CodeReviewApiService {
   apiUrl = urls.codeReviewUrl;
@@ -15,10 +16,13 @@ export class CodeReviewApiService {
     }
   }
 
-  async sendMessage(codeReviewId: number): Promise<CodeReview> {
+  async sendMessage(
+    codeReviewId: number,
+    message: CodeReviewMessageDTO
+  ): Promise<CodeReview> {
     let url = this.apiUrl + `/${codeReviewId}/message`;
     try {
-      const response = await http.post(url);
+      const response = await http.post(url, message);
       return response.data.data;
     } catch (error) {
       throw error.response.data;
